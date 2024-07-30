5bfd6c106e0a1d01be9a68b009ccc749b1170cb8
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2024-07-30 16:36:03 -0400

    Finish permutation

--- src/main/java/week2/queue/Permutation.java
+++ src/main/java/week2/queue/Permutation.java
@@ -1,7 +1,26 @@
 package week2.queue;
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
+        if (args.length < 1) {
+            throw new IllegalArgumentException("Usage: java Permutation k");
+        }
 
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+
+        // Read strings from standard input and add them to the randomized queue
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            rq.enqueue(item);
+        }
+
+        // Dequeue and print k items
+        for (int i = 0; i < k; i++) {
+            StdOut.println(rq.dequeue());
+        }
     }
 }
